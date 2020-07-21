f43504f52a58a73c80e06fa834bd96842b824158
Anil Dhurjaty <adhurjaty@gmail.com>
2020-07-21 16:45:07 -0700

    adds nav links

--- view/src/components/Header.tsx
+++ view/src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Link } from 'react-router-dom';
 
 const NavBar = styled.div`
     display: flex;
@@ -12,10 +13,28 @@ const NavBar = styled.div`
     margin-bottom: 10px;
 `;
 
+const Links = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+`
+
+const NavLink = styled.a`
+    background-color: gray;
+    color: white;
+    padding: 0.6em 1.0em;
+    text-decoration: none;
+    margin-right: 10px;
+`
+
 function Header(): JSX.Element {
     return (
         <NavBar>
             <span><b>Wisely Reservation Manager</b></span>
+            <Links>
+                <NavLink href="/inventory">Inventory</NavLink>
+                <NavLink href="/reservations">Reservations</NavLink>
+            </Links>
         </NavBar>
     )
 }
